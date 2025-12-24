72c3abd57c68e08ef787cc56512fb93ad8a142f5
William <monkeykingisback@gmail.com>
2025-12-24 16:02:31 -0600

    feat: Remove DelitDiT and DelitLoss, and add DelitWanLoRA.

--- diffsynth/scripts/__init__.py
+++ diffsynth/scripts/__init__.py
@@ -3,18 +3,14 @@ FaceOLAT Delit Training System
 """
 
 from .hdr_codec import HDRCodec, HDRCodecTorch
-from .delit_model import DelitDiT
-from .delit_loss import DelitLoss
 from .delit_dataset import FaceOLATDelitDataset
+from .delit_model_lora import DelitWanLoRA
 
 __version__ = "0.1.0"
 
 __all__ = [
     'HDRCodec',
     'HDRCodecTorch',
-    'SimplifiedDelitModel',
-    'DelitDiT',
-    'SimplifiedDelitLoss',
-    'DelitLoss',
     'FaceOLATDelitDataset',
+    'DelitWanLoRA',
 ]
